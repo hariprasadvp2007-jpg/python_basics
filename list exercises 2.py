c2d567a2a59352dad07034c32992b810b48e3334
hariprasadvp <hariprasadvp2007@gmail.com>
2025-11-06 23:07:43 +0530

    added list exercises 2

---
+++ list exercises 2.py
@@ -0,0 +1,29 @@
+# Write a program to create,append and remove lists in python using Numpy
+
+# To create a list:
+fruits = ["apple", "banana", "pineapple", "mango"]
+print("The list has been created")
+print(fruits)
+
+# To append items to a list:
+fruits.append("orange")
+print("list after appending 'orange' :")
+print(fruits)
+
+# To remove items by string from a list:
+fruits.remove("banana")
+print("list after removing 'banana' :")
+print(fruits)
+
+# To remove items by index from a list:
+fruits.pop(2)
+print("list after removing the element at index 2 :")
+print(fruits)
+
+# To delete the list:
+del fruits
+print("The list has been deleted")
+
+
+
+
